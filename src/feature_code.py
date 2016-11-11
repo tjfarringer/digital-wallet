f2c44072cc919bfbe408baf97ba4d0849ad46ff5
tjfarringer <tjfarrin@gmail.com>
2016-11-10 22:39:39 -0500

    second edit

--- src/feature_code.py
+++ src/feature_code.py
@@ -17,8 +17,6 @@ outfile_3 = sys.argv[5]
 ################################################
 import pandas as pd
 import numpy as np
-import seaborn as sns
-import matplotlib.pyplot as plt
 import datetime
 
 
